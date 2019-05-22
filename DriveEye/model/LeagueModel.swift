b8b670e03a8f01418277998aef5119d5010b5814
ashraf <ashraf@abanoubs-mac.local>
2019-05-22 12:43:05 +0200

    add league classes

---
+++ DriveEye/model/LeagueModel.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  DriveEye
+//
+//  Created by ashraf on 5/22/19.
+//  Copyright © 2019 java. All rights reserved.
+//
+
+import Foundation
+class LeagueModel {
+    func getLeagues() -> League {
+        
+    }
+}
