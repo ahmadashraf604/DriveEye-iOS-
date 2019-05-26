d32ebeede061d99ec57321ff8049a18432f75853
ashraf <ashraf@Abanoubs-Mac.local>
2019-05-26 13:58:40 +0200

    seasonMasterDetails

---
+++ DriveEye/pojo/Season.swift
@@ -0,0 +1,15 @@
+//
+//  Season.swift
+//  DriveEye
+//
+//  Created by ashraf on 5/26/19.
+//  Copyright © 2019 java. All rights reserved.
+//
+
+import Foundation
+class Season {
+    var endDate:String?
+    var startDate:String?
+    var users:[User]?
+
+}
